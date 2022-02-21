dcb1232922e79515ca3924cd219ee81d6f47630d
LukeKnezCK <LukeKnez1386@protonmail.com>
2022-02-20 19:49:14 -0800

    Problem 28: Implement strStr() Updated

--- leetcode28.py
+++ leetcode28.py
@@ -1,26 +1,11 @@
-def strStr(haystack: str, needle: str) -> int:
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
         if len(needle) == 0:
             return 0
         elif len(haystack) == 0:
             return -1
         else:
-            y = 0
-            while y < len(haystack):
-                if haystack[y] == needle[0]:
-                    for x in range(len(needle)):
-                        if y < len(haystack):
-                            if haystack[y] != needle[x]:
-                                y -= x -1
-                                break
-                            elif x == len(needle)-1:
-                                return y - len(needle)+1
-                            else:
-                                y += 1
-                        else:
-                            return -1
-                else:
-                    y +=1
+            for i in range(len(haystack) -len(needle)+1):
+                if haystack[i:i+len(needle)] == needle:
+                    return i
             return -1
-
-strStr("mississippi"
-,"issip")
