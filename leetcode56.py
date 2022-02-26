bef3cd2fa9275d0d2f96966a549748d848e86a60
LukeKnezCK <LukeKnez1386@protonmail.com>
2022-02-26 15:25:56 -0800

    Problem 56: Merge Intervals

--- leetcode56.py
+++ leetcode56.py
@@ -1,19 +1,21 @@
 class Solution:
-    def merge(self, intervals: list[list[int]]) -> list[list[int]]:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals)
         if len(intervals) == 1:
             return intervals
         x = 0
         results = []
         while x <= len(intervals)-1:
-            if x == len(intervals):
+            if x == len(intervals)-1:
                 results.append(intervals[x])
                 x+=1
-            elif intervals[x][1] >= intervals[x+1][0]:
-                intervals[x+1][0] = intervals[x][0]
+            elif intervals[x][1] >= intervals[x+1][0] and intervals[x][0] <= intervals[x+1][1]:
+                if intervals[x+1][0] >= intervals[x][0]:
+                    intervals[x+1][0] = intervals[x][0]
+                if intervals[x][1] > intervals[x+1][1]:
+                    intervals[x+1][1] = intervals[x][1]
                 x += 1
             else:
                 results.append(intervals[x])
                 x += 1
         return results
-
-Solution.merge(Solution, [[1,3],[2,6],[8,10],[15,18]])
