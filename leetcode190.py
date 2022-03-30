805789bb5ff7fc10b5fc4fb398e1f6283ee353c5
LukeKnezCK <LukeKnez1386@protonmail.com>
2022-03-29 23:09:04 -0700

    Problem 190: Reverse Bits

---
+++ leetcode190.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+        for i in range(32):
+            ans = (ans << 1) + (n & 1)
+            n >>= 1
+        return ans
+
+Solution.reverseBits(Solution, 0b00000010100101000001111010011100)
