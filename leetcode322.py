ab9aef26ca8c6e9148b75d9a6cc4ef62b192a09f
LukeKnezCK <LukeKnez1386@protonmail.com>
2022-04-20 14:36:56 -0700

    Problem 322: Coin Change

--- leetcode322.py
+++ leetcode322.py
@@ -1,3 +1,34 @@
+from tkinter import W
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        
+        layer = 0
+        value1 = [0]
+        value2 = []
+        visits = [False] * (amount+1)
+        visits[0] = True
+        
+        while value1:
+            layer += 1
+            for v in value1:
+                for coin in coins:
+                    newval = v + coin
+                    if newval == amount:
+                        return layer
+                    elif newval > amount:
+                        continue
+                    elif not visits[newval]:
+                        visits[newval] = True
+                        value2.append(newval)
+            value1, value2 = value2, []
+        return -1
+
+
+#Tried a dfs recursive thing, it didnt work out :p
 class Solution:
     def coinChange(self, coins: list[int], amount: int) -> int:
         if amount == 0:
