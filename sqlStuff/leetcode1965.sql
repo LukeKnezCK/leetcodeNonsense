04373687f692e20d7cfd502a1f34fed1727aabc6
LukeKnezCK <LukeKnez1386@protonmail.com>
2022-04-24 01:17:47 -0700

    Problem 1965: Employees with missing info

---
+++ sqlStuff/leetcode1965.sql
@@ -0,0 +1,6 @@
+SELECT employee_id from Employees
+WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
+UNION
+SELECT employee_id from Salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id
