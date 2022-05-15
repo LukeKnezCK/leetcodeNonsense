c1bdf41c9d47b271ff3cdc1b51a02461d74081bb
LukeKnezCK <LukeKnez1386@protonmail.com>
2022-05-15 12:37:46 -0700

    Problem 387: First Unique Character in String Alt

--- javaStuff/leetcode387.java
+++ javaStuff/leetcode387.java
@@ -14,3 +14,23 @@ class Solution {
         return -1;
     }
 }
+
+//alt solution
+class Solution {
+    public int firstUniqChar(String s) {
+        LinkedHashMap<Character, Integer> encounters = new LinkedHashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            Character encounter = s.charAt(i);
+            if(encounters.containsKey(encounter)) encounters.replace(encounter,-1);
+            else encounters.put(encounter, i);
+        }
+        int result = -1;
+        for(Map.Entry<Character, Integer> mapElement: encounters.entrySet()){
+            if(mapElement.getValue()>-1){
+                result = mapElement.getValue();
+                return result;
+            }
+        }
+        return result;
+    }
+}
